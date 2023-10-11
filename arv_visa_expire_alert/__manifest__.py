2d3732b1f34e0f62ab1e28d7c9996528bf544786
Aravind <aravindu28@gmail.com>
2023-10-11 10:56:51 +0530

    [UPDATE] changed thumbnail,icon

--- arv_visa_expire_alert/__manifest__.py
+++ arv_visa_expire_alert/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Visa Expiration Notification',
-    'version': '16.0.1',
+    'version': '16.0.2',
     'sequence': 1,
     'website': 'aravindu28@gmail.com',
     'category': 'Extra Tools',
